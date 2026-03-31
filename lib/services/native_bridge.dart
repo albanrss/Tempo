b5b9a95e206c79de5fe470ed54f4cc14c2a2a500
albanrss <alban.roussee@epitech.eu>
2026-03-31 20:51:25 +0200

    refactor(native bridge): simplify formatting of isAccessibilityServiceEnabled method

--- lib/services/native_bridge.dart
+++ lib/services/native_bridge.dart
@@ -4,8 +4,7 @@ class NativeBridge {
   static const _channel = MethodChannel('albanrss.tempo/app_blocker');
 
   static Future<bool> isAccessibilityServiceEnabled() async {
-    return await _channel.invokeMethod<bool>('isAccessibilityServiceEnabled') ??
-        false;
+    return await _channel.invokeMethod<bool>('isAccessibilityServiceEnabled') ?? false;
   }
 
   static Future<void> openAccessibilitySettings() async {
